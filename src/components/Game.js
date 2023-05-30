e21e3cd228aa6282d00269add9a25e8b93a3f9db
ViniBapts <vinicius.baptistella96@gmail.com>
2023-05-30 11:29:56 -0300

    first commit

---
+++ src/components/Game.js
@@ -0,0 +1,71 @@
+import { useState, useRef } from "react";
+import "./Game.css";
+
+export const Game = ({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}) => {
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter);
+
+    setLetter("");
+    letterInputRef.current.focus()
+  };
+
+  return (
+    <div className="game">
+      <p className="point">
+        <span>Score: {score}</span>
+      </p>
+      <h1>Guess the word</h1>
+      <h3 className="tip">
+        Tip on the word: <span>{pickedCategory}</span>
+      </h3>
+      <p>You still have {guesses} tries.</p>
+      <div className="wordcontainer">
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          )
+        )}
+      </div>
+      <div className="lettercontainer">
+        <p>Try to guess a letter of the word: </p>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name=""
+            letter
+            maxLength="1"
+            required
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+          />
+
+          <button>Play!</button>
+        </form>
+      </div>
+      <div className="wrongletterContainer">
+        <p>Letters already used: </p>
+        {wrongLetters.map((letter, i) => (
+          <span key={i}>{letter},</span>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  car: ["Engine", "Door", "Hood", "Tire", "Antenna"],
+  fruit: ["Banana", "Apple", "Pear", "Papaya", "Orange"],
+  corpo: ["Arm", "Leg", "Brain", "Neck", "Eyes"],
+  computador: ["Mouse", "Keyboard", "Monitor", "Cabinet"],
+  programação: ["Language", "Framework", "JavaScript", "React"],
+  alimento: ["Rice", "Beans", "Meat", "Milk", "Egg"],
+};
